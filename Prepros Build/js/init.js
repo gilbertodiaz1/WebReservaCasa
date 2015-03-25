8423901b24c613d8de11fc06ef8518b552c3c604
José Gilberto DÍaz Pinto <jose.gilbertopinto@gmail.com>
2015-03-25 08:13:53 -0430

    Archivos Nuevos

---
+++ Prepros Build/js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+      $('.slider').slider();
+    });
+
+ $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
