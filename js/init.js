c3a91d4c4e00dc1b9d6bd852c22f0d293eac9f6f
José Gilberto DÍaz Pinto <jose.gilbertopinto@gmail.com>
2015-03-26 08:36:50 -0430

    Cambios Varios

--- js/init.js
+++ js/init.js
@@ -13,3 +13,8 @@ $(document).ready(function(){
  $(document).ready(function(){
     $('.materialboxed').materialbox();
   });
+
+ $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
